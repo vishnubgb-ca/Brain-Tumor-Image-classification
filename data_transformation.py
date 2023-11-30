fa79e5b72e29c4947f7cf543686943eb06ba12ac
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-11-30 15:21:00 +0530

    Update data_transformation.py

--- data_transformation.py
+++ data_transformation.py
@@ -6,7 +6,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 import torch
-import pickle
+import dill as pickle
 
 def transform_data():
     glioma_tumor_image_urls,meningioma_tumor_image_urls,no_tumor_image_urls,pituitory_tumor_image_urls = visualise_image()
