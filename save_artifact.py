0fe77ed48626899c1aa47c94dccc443255064552
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-11-30 15:02:09 +0530

    Update save_artifact.py

--- save_artifact.py
+++ save_artifact.py
@@ -3,6 +3,8 @@ import torch
 
 def save_artifact():
     model_dataset = transform_data()
-    torch.save(model_dataset, "test.pt")
+    # torch.save(model_dataset, "test.pt")
     # model_dataset = torch.load('test.pt')
+    with open('model_dataset.pkl', 'wb') as f:
+        pickle.dump(model_dataset, f)
     return model_dataset
