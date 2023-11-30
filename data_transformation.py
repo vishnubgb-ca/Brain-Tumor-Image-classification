98cb4c5e78527cca8d9082a40074715951d272a7
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-11-30 14:52:19 +0530

    Update data_transformation.py

--- data_transformation.py
+++ data_transformation.py
@@ -1,4 +1,4 @@
-from datavisualization import visualise_image
+from data_visualisation import visualise_image
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Dataset
@@ -16,18 +16,40 @@ def transform_data():
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    tumor_list = [glioma_tumor_image_urls,meningioma_tumor_image_urls,no_tumor_image_urls,pituitory_tumor_image_urls]
-    images = []
-    for lst in tumor_list:
-        for url in lst:
-            response = requests.get(url)
-            image = Image.open(BytesIO(response.content))
-            images.append(image)
+    # tumor_list = [glioma_tumor_image_urls,meningioma_tumor_image_urls,no_tumor_image_urls,pituitory_tumor_image_urls]
+    # images = []
+    # for lst in tumor_list:
+    #     for url in lst:
+    #         response = requests.get(url)
+    #         image = Image.open(BytesIO(response.content))
+    #         images.append(image)
     
-    transformed_images = [data_transform(img) for img in images]
-    model_dataset = torch.stack(transformed_images)
-    return model_dataset
+    # transformed_images = [data_transform(img) for img in images]
+    # model_dataset = torch.stack(transformed_images)
+    
 
     # model_dataset = datasets.ImageFolder(path, transform=data_transform)
 
+    def download_image(url):
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+        return img
+    
+    class CustomImageDataset(Dataset):
+        def __init__(self, image_urls, transform=None):
+            self.image_urls = image_urls
+            self.transform = transform
+
+        def __len__(self):
+            return len(self.image_urls)
+
+        def __getitem__(self, idx):
+            img = download_image(self.image_urls[idx])
+            if self.transform:
+                img = self.transform(img)
+            return img
+    
+    all_image_urls = glioma_tumor_image_urls+meningioma_tumor_image_urls+no_tumor_image_urls+pituitory_tumor_image_urls
+    model_dataset = CustomImageDataset(all_image_urls, transform=data_transform)
+    return model_dataset
 transform_data()
